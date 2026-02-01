c20a9cd2647b7611fe338c30407afbd86a611c5f
mohammadhossein-sa <computermhs100@gmail.com>
2026-02-01 15:15:41 +0330

    feat(model): add observer pattern support to BlogModel

--- public/js/model.js
+++ public/js/model.js
@@ -5,6 +5,23 @@ class BlogModel {
     this.apiBaseUrl = 'http://localhost:3001/api/posts';
     this.isLoading = false;
   }
+
+  // Observer pattern implementation
+  addObserver(observer) {
+    this.observers.push(observer);
+  }
+
+  removeObserver(observer) {
+    this.observers = this.observers.filter(obs => obs !== observer);
+  }
+
+  notifyObservers(event, data) {
+    this.observers.forEach(observer => {
+      if (observer[event]) {
+        observer[event](data);
+      }
+    });
+  }
 }
 
 window.BlogModel = BlogModel;
