acf3c3a9f5461d83e1b37bcf50d0b539169298eb
mohammadhossein-sa <computermhs100@gmail.com>
2026-02-01 16:15:05 +0330

    feat(controller): add base BlogController structure and initialization flow

---
+++ public/js/controller.js
@@ -0,0 +1,84 @@
+class BlogController {
+  constructor(model, view) {
+    this.model = model;
+    this.view = view;
+    this.isInitialized = false;
+
+    // Bind methods
+    this.initialize = this.initialize.bind(this);
+    this.handlePostCreate = this.handlePostCreate.bind(this);
+    this.handlePostUpdate = this.handlePostUpdate.bind(this);
+    this.handlePostDelete = this.handlePostDelete.bind(this);
+    this.handlePostEdit = this.handlePostEdit.bind(this);
+    this.handleLoadingStart = this.handleLoadingStart.bind(this);
+    this.handleLoadingEnd = this.handleLoadingEnd.bind(this);
+    this.handleError = this.handleError.bind(this);
+    this.handlePostsLoaded = this.handlePostsLoaded.bind(this);
+    this.handlePostCreated = this.handlePostCreated.bind(this);
+    this.handlePostUpdated = this.handlePostUpdated.bind(this);
+    this.handlePostDeleted = this.handlePostDeleted.bind(this);
+  }
+
+  async initialize() {
+    if (this.isInitialized) return;
+
+    this.setupModelObservers();
+    this.setupViewObservers();
+
+    await this.view.initialize();
+    await this.loadPosts();
+
+    this.isInitialized = true;
+  }
+
+  setupModelObservers() {
+    this.model.addObserver({
+      onPostsLoaded: this.handlePostsLoaded,
+      onPostCreated: this.handlePostCreated,
+      onPostUpdated: this.handlePostUpdated,
+      onPostDeleted: this.handlePostDeleted,
+      onError: this.handleError,
+      onLoadingStart: this.handleLoadingStart,
+      onLoadingEnd: this.handleLoadingEnd,
+    });
+  }
+
+  setupViewObservers() {
+    this.view.addObserver({
+      onViewInitialized: this.handleViewInitialized,
+      onPostCreate: this.handlePostCreate,
+      onPostUpdate: this.handlePostUpdate,
+      onPostDelete: this.handlePostDelete,
+      onPostEdit: this.handlePostEdit,
+    });
+  }
+
+  async loadPosts() {
+    await this.model.loadPosts();
+  }
+
+  handleViewInitialized() {}
+
+  handlePostsLoaded(posts) {
+    this.view.renderPosts(posts);
+  }
+
+  handlePostCreated(newPost) {
+    this.view.clearForm();
+    this.loadPosts();
+  }
+
+  handleLoadingStart() {
+    this.view.showLoading();
+  }
+
+  handleLoadingEnd() {
+    this.view.hideLoading();
+  }
+
+  handleError(message) {
+    this.view.showError(message);
+  }
+}
+
+window.BlogController = BlogController;
